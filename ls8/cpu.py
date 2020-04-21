d0265656c0656153d4fad4790567f7de17c9b4f5
Ali Rajaii <rajaii_a@yahoo.com>
2020-04-21 17:34:33 -0400

    working but have questions about ram vs register

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -13,32 +13,37 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-
+        
         address = 0
 
-        # For now, we've just hardcoded a program:
+        filename = sys.argv[1]
+        
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        with open(filename) as f:
+            for instruction in f:
+                instruction = instruction.split('#')
+                instruction = instruction[0].strip()
+                if instruction == '':
+                    continue
+                #MAYBE:
+                # self.ram[address] = int(instruction, 2)
+                
+                self.ram[address] = int(instruction, 2)
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                address += 1
+
+        
+
 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.ram[reg_a] *= self.ram[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -74,6 +79,7 @@ class CPU:
         HLT = 0b00000001
         LDI = 0b10000010
         PRN = 0b01000111
+        MUL = 0b10100010
 
         while running:
             ir = self.ram[self.pc]
@@ -88,9 +94,16 @@ class CPU:
                 self.pc += 2
             elif ir == HLT:
                 running = False
+                #handle mul case
+            elif ir == MUL:
+                #call alu passing in params not sure if correct cuz registers not ram
+                self.alu("MUL", operand_a, operand_b)
+                #increment self.pc
+                self.pc += 3
             else:
-                print("Unknown instruction")
-		        running = False
+                print('unknown command')
+                running = False
+             
 
 
 
